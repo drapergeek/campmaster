67c556c291b6481ceb002f4fc2cd1355cde2b794
Jason Draper <jason@drapergeek.com>
2012-11-18 15:41:20 -0500

    Add config for sending emails in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -66,5 +66,19 @@ Campmaster::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'campmaster-staging.herokuapp.com' }
+  HOST = 'campmaster-staging.herokuapp.com'
+  config.action_mailer.default_url_options = { :host => HOST}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_charset = "utf-8"
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => HOST,
+    :address =>  'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auth => true
+  }
 end
